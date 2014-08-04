7a3e246b98f181be49b6ce20442521e99eb0daaf
Adrian Herrera <adrian.herrera@OMSOLA-036301.local>
2014-08-04 11:00:00 -0500

    Import checkinstall version 1.0.1

---
+++ cookbooks/checkinstall/metadata.rb
@@ -0,0 +1,13 @@
+name             "checkinstall"
+maintainer       "Rostyslav Fridman"
+maintainer_email "rostyslav.fridman@gmail.com"
+license          "Apache 2.0"
+description      "Installs/Configures checkinstall"
+long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
+version          "1.0.1"
+
+supports "centos"
+supports "debian"
+supports "fedora"
+supports "redhat"
+supports "ubuntu"
