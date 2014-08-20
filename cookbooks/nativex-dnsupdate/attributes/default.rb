3ffa51f3ed01b07066d353c012c984f380562d1f
adrian.herrera <adrian.herrera@nativex.com>
2014-08-20 11:29:36 -0500

    moved nativex-dnsupdate attribute back to nativexintern.com

--- cookbooks/nativex-dnsupdate/attributes/default.rb
+++ cookbooks/nativex-dnsupdate/attributes/default.rb
@@ -6,7 +6,7 @@ default['nativex-dnsupdate']['int_domain'] = 'nativexintern.com'
 default['nativex-dnsupdate']['int_zone_id'] = 'Z1NZHUXT02UQJ'
 
 # External Domain Name
-default['nativex-dnsupdate']['ext_domain'] = 'teamfreeze.com'
+default['nativex-dnsupdate']['ext_domain'] = 'nativexintern.com'
 
 # external ZoneID
-default['nativex-dnsupdate']['ext_zone_id'] = 'ZPCX17R5ZD3SC'
+default['nativex-dnsupdate']['ext_zone_id'] = 'Z1NZHUXT02UQJ'
