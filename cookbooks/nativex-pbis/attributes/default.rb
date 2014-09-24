3cbaf0430efd396905f68851d4cab3847e109b23
adrian.herrera <adrian.herrera@nativex.com>
2014-09-24 14:55:33 -0500

    update to pbis cookbook removing unneeded username and password attribute

--- cookbooks/nativex-pbis/attributes/default.rb
+++ cookbooks/nativex-pbis/attributes/default.rb
@@ -1,9 +1,3 @@
 #
 # Domain Name
 default['nativex-pbis']['domain_name'] = 'teamfreeze.com'
-
-# AD Username
-default['nativex-pbis']['ad_username'] = 'nil'
-
-# AD Password
-default['nativex-pbis']['ad_password'] = 'nil'
