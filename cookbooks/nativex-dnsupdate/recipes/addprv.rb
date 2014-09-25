743d98501199dd574e41f142a45893c6db708764
adrian.herrera <adrian.herrera@nativex.com>
2014-09-25 16:49:31 -0500

    update to dnsupdate to support ubunut systems

--- cookbooks/nativex-dnsupdate/recipes/addprv.rb
+++ cookbooks/nativex-dnsupdate/recipes/addprv.rb
@@ -13,7 +13,7 @@ aws = Chef::EncryptedDataBagItem.load("credentials", "aws")
 full_nodename = "#{node.name}-prv.#{node['nativex-dnsupdate']['int_domain']}"
 route53_record "create A record" do
   name                   full_nodename
-  value                  node[:ec2][:local_ipv4] 
+  value                  node[:ec2][:ipaddress] 
   type                   "A"
   zone_id                node['nativex-dnsupdate']['int_zone_id']
   aws_access_key_id      aws["aws_access_key_id"]
