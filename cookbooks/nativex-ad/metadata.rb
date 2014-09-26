de7e06584f2c1cc8b7a89b994ba60f447b42492f
Corey Hemminger <corey.hemminger@nativex.com>
2014-09-26 14:29:14 -0500

    adding files

---
+++ cookbooks/nativex-ad/metadata.rb
@@ -0,0 +1,8 @@
+name             'nativex-ad'
+maintainer       'NativeX'
+maintainer_email 'corey.hemminger@nativex.com'
+license          'All rights reserved'
+description      'Installs/Configures nativex-ad'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+depends  "windows_ad"
