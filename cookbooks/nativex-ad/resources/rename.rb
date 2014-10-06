67b5ca7c52e6e1e9138fa7877fc95cbed2e90efa
adrian.herrera <adrian.herrera@nativex.com>
2014-10-06 10:57:56 -0500

    fixed rename resource so attribute kind is set with the correct syntax

--- cookbooks/nativex-ad/resources/rename.rb
+++ cookbooks/nativex-ad/resources/rename.rb
@@ -2,6 +2,6 @@ actions :rename
 default_action :rename
 
 attribute :name, :kind_of => String, :name_attribute => true
-attribute :hostname, kind_of => String
+attribute :hostname, :kind_of => String
 attribute :domain_user, :kind_of => String
 attribute :domain_pass, :kind_of => String
