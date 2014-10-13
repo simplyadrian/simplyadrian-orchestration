89c80c0f3a2fd9e12f809bb03872bd48401d540c
adrian.herrera <adrian.herrera@nativex.com>
2014-10-13 10:11:07 -0500

    finalized ohai-aws plugin installer and updated domain-controller role to remove the promotion recipe until domaincontroller provider is completed. It has been decided that the promotion to DC will be done manually for now.

--- cookbooks/ohai-aws/recipes/default.rb
+++ cookbooks/ohai-aws/recipes/default.rb
@@ -1,5 +1,5 @@
 #
-# Cookbook Name:: nativex-ad
+# Cookbook Name:: ohai-aws
 # Recipe:: default 
 #
 # Copyright 2014, NativeX
@@ -7,4 +7,8 @@
 # All rights reserved - Do Not Redistribute
 #
 
-include_recipe 'ohai::default'
+include_recipe 'ohai-aws::awsplugin'
+include_recipe 'ohai-aws::cloudformationplugin'
+include_recipe 'ohai-aws::elasticacheplugin'
+include_recipe 'ohai-aws::elbplugin'
+include_recipe 'ohai-aws::rdsplugin'
