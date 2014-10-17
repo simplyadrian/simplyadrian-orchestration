17ef11a6ade1abe1bf11ac9beb9d8f922dca82f7
Derek Bromenshenkel <derek.bromenshenkel@nativex.com>
2014-10-17 15:25:57 -0500

    Refactored all references of nativex-<cookbook> to <cookbook>-nativex in cookbooks/mysql-nativex

--- cookbooks/mysql-nativex/recipes/default.rb
+++ cookbooks/mysql-nativex/recipes/default.rb
@@ -1,5 +1,5 @@
 #
-# Cookbook Name:: nativex-mysql
+# Cookbook Name:: mysql-nativex
 # Recipe:: default
 #
 # Copyright 2014, NativeX
@@ -7,4 +7,4 @@
 # All rights reserved - Do Not Redistribute
 #
 
-include_recipe "nativex-mysql::custom_cnf"
+include_recipe "mysql-nativex::custom_cnf"
