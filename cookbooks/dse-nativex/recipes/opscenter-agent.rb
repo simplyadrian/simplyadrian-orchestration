bd4bd484ddd8216ed9760538a655280689bdfecd
Derek Bromenshenkel <derek.bromenshenkel@nativex.com>
2014-10-20 16:44:38 -0500

    Refinements to opscenter-agent recipe. Better error handling, proper log messages, and using a resource guard instead of unless statement.

--- cookbooks/dse-nativex/recipes/opscenter-agent.rb
+++ cookbooks/dse-nativex/recipes/opscenter-agent.rb
@@ -11,26 +11,26 @@ template "#{node['opscenter-agent']['conf_dir']}/address.yaml" do
  end
 
 # Make sure the cluster exists in OpsCenter.  Create it if it does not.
-unless node['opscenter-agent']['is_opscenter_cluster_configured']
-  ruby_block "opscenter-cluster-existence" do
-    block do
-      require 'net/http'
-      require 'openssl'
-      require 'json'
-      uri = URI("#{node['opscenter-agent']['opscenter_api_uri']}cluster-configs")
-      Chef::Log.warn("OpsCenter API URL: #{uri}.")
-    
-      req = Net::HTTP::Get.new(uri.path)
-      req.basic_auth 'user', 'user'
-    
-      res = Net::HTTP.start(uri.hostname, uri.port, {use_ssl: uri.scheme == 'https', verify_mode: OpenSSL::SSL::VERIFY_NONE}) {|http|
-        http.request(req)
-      }
+ruby_block "opscenter-cluster-existence" do
+  block do
+    require 'net/http'
+    require 'openssl'
+    require 'json'
+    uri = URI("#{node['opscenter-agent']['opscenter_api_uri']}cluster-configs")
+    Chef::Log.debug("OpsCenter API URL: #{uri}.")
+  
+    req = Net::HTTP::Get.new(uri.path)
+    req.basic_auth 'user', 'user'
+  
+    res = Net::HTTP.start(uri.hostname, uri.port, {use_ssl: uri.scheme == 'https', verify_mode: OpenSSL::SSL::VERIFY_NONE}) {|http|
+      http.request(req)
+    }
+	if res != nil && (res.kind_of?(Net::HTTPSuccess) || res.kind_of?(Net::HTTPOK))
       current_clusters = JSON.parse(res.body)
-    
+      
       @normalized_cluster_name = node['cassandra']['cluster_name'].gsub(/[ -]/, '_') #OpsCenter doesn't like spaces and dashes...Nasty trick they do and convert to underscore
       if !current_clusters.keys.include?(@normalized_cluster_name)
-        Chef::Log.warn("Adding new cluster #{node['cassandra']['cluster_name']} to OpsCenter.")
+        Chef::Log.info("Adding new cluster '#{node['cassandra']['cluster_name']}' using name '#{@normalized_cluster_name}' to OpsCenter.")
         @payload = { "cassandra" => { "seed_hosts" => node['cassandra']['seeds'] }, "jmx" => { "port" => "7199" } }.to_json
         put_req = Net::HTTP::Post.new(uri.path)
         put_req.body = @payload
@@ -38,14 +38,21 @@ unless node['opscenter-agent']['is_opscenter_cluster_configured']
         put_res = Net::HTTP.start(uri.hostname, uri.port, {use_ssl: uri.scheme == 'https', verify_mode: OpenSSL::SSL::VERIFY_NONE}) {|http|
           http.request(put_req)
         }
-        node.set['opscenter-agent']['is_opscenter_cluster_configured'] = true
+        if put_res != nil && (put_res.kind_of?(Net::HTTPSuccess) || put_res.kind_of?(Net::HTTPOK))
+          node.set['opscenter-agent']['is_opscenter_cluster_configured'] = true
+        else
+          Chef::Log.warn("Got a non-successful response from OpsCenter while adding cluster! StatusCode: #{put_res.code}; Message: #{put_res.message}")
+        end
       else
         Chef::Log.debug("Cluster #{node['cassandra']['cluster_name']} already exists in OpsCenter.")
         node.set['opscenter-agent']['is_opscenter_cluster_configured'] = true # no need to keep running this code
       end
+    else
+      Chef::Log.warn("Got a non-successful response from OpsCenter while querying cluster configuration! StatusCode: #{res.code}; Message: #{res.message}")
     end
-    action :run
   end
+  not_if { node['opscenter-agent']['is_opscenter_cluster_configured'] }
+  action :run
 end
 
 #Restart the agent
