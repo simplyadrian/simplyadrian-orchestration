573dfa4f6190535c869cfa5b24e3eda82be3031a
Derek Bromenshenkel <derek.bromenshenkel@nativex.com>
2014-10-20 15:15:06 -0500

    Fixed issue with AWS ohai custom plugin blowing up on first run. See: https://stackoverflow.com/questions/25415967/how-to-distribute-a-custom-ohai-plugin/25419778#25419778 , and more specifically: https://github.com/TYPO3-cookbooks/openvz/blob/8204d6a53a6586fb8a4c6ea159e85fff3fe32c2f/recipes/ohai.rb#L71-L89 Essentially, use the name specified by 'provides <blah>' as the plugin name in the reload resource, not the attribute in the plugin definition.

--- cookbooks/ohai-nativex/recipes/awsplugin.rb
+++ cookbooks/ohai-nativex/recipes/awsplugin.rb
@@ -8,13 +8,13 @@
 #
 
 ohai 'reload_aws' do
-  plugin 'AwsRegion'
+  plugin 'aws'
   action :nothing
 end
 
 cookbook_file "#{node['ohai']['plugin_path']}/aws.rb" do
   source 'aws.rb'
-  notifies :reload, 'ohai[reload_aws]', :delayed
+  notifies :reload, 'ohai[reload_aws]'
 end
 
 include_recipe 'ohai::default'
