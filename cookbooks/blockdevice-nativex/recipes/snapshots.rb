a6875f2138634987c92854d569e322e7d13b5aba
adrian.herrera <adrian.herrera@nativex.com>
2014-11-03 15:46:29 -0600

    added description to snapshot recipe as well as fixed pruning so snapshots will be pruned when attribute value is met.

--- cookbooks/blockdevice-nativex/recipes/snapshots.rb
+++ cookbooks/blockdevice-nativex/recipes/snapshots.rb
@@ -9,7 +9,7 @@ if node[:blockdevice_nativex][:ec2] || node[:cloud][:provider] == 'ec2'
   devices = node['aws']['ebs_volume'].values.to_s.scan(/vol-[a-zA-Z0-9]+/)
 
   devices.each do |values|
-    aws_ebs_volume "create ebs snapshots" do
+    aws_ebs_volume "#{node.hostname}_#{node.chef_environment}" do
       aws_access_key aws['aws_access_key_id']
       aws_secret_access_key aws['aws_secret_access_key']  
       size node[:blockdevice_nativex][:ebs][:size]
@@ -17,6 +17,7 @@ if node[:blockdevice_nativex][:ec2] || node[:cloud][:provider] == 'ec2'
       snapshots_to_keep node[:blockdevice_nativex][:snapshots_to_keep]
       action :snapshot
       volume_id values
+      description "snapshot of volume attached to #{node.hostname}_#{node.chef_environment}"
       ignore_failure true # if this fails, continue to unfreeze and unlock
     end
   end
@@ -25,7 +26,16 @@ if node[:blockdevice_nativex][:ec2] || node[:cloud][:provider] == 'ec2'
   #  command "xfs_freeze -u #{node.db_snapshot.ebs_vol_dev}"
   #end
 
-  aws_ebs_volume "prune ebs snapshots" do
+  aws_ebs_volume "#{node.hostname}_#{node.chef_environment}" do
     action :prune
   end
+end
+
+aws_resource_tag 'tag_snapshots' do
+  aws_access_key aws['aws_access_key_id']
+  aws_secret_access_key aws['aws_secret_access_key']
+  resource_id devices
+  tags({"Name" => node.hostname,
+        "Environment" => node.chef_environment})
+  action [:add, :update]
 end
