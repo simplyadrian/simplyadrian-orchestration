ba136f59ab386faa14b7b02103f18efc0485d9ff
Derek Bromenshenkel <derek.bromenshenkel@nativex.com>
2014-11-26 15:51:25 -0600

    Force dse cookbook to use java v1.24.0 (JDK 7u65)

--- cookbooks/dse-nativex/metadata.rb
+++ cookbooks/dse-nativex/metadata.rb
@@ -6,7 +6,7 @@ description      'Installs/Configures DataStax Enterprise'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends "java", "~> 1.14"
+depends "java", "= 1.24.0"
 depends "yum", "~> 3.0"
 depends "limits"
 depends "line"
