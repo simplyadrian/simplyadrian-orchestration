ee067b2a20c4cadde5893772ab4dfa224a4538bd
adrian.herrera <adrian.herrera@nativex.com>
2014-11-29 13:45:27 -0600

    playing with glusterfs

--- cookbooks/glusterfs-nativex/metadata.rb
+++ cookbooks/glusterfs-nativex/metadata.rb
@@ -1,12 +1,13 @@
-maintainer 'Adrian Herrera'
+name             'glusterfs-nativex'
+maintainer       'NativeX'
 maintainer_email 'adrian.herrera@nativex.com'
-license 'All rights reserved'
-description 'Installs and configures glusterfs as a server or client.'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version '0.1.0'
-recipe 'glusterfs', 'Installs glusterfs::server and glusterfs::client.'
-recipe 'glusterfs::server', 'Installs and configures glusterfs server.'
-recipe 'glusterfs::peer', 'Installs and configures glusterfs server as a peer.'
-recipe 'glusterfs::client', 'Installs and configures glusterfs client and mounts volumes.'
+license          'All rights reserved'
+description      'Installs/Configures glusterfs-nativex'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+recipe 'glusterfs-nativex', 'Installs glusterfs::server and glusterfs::client.'
+recipe 'glusterfs-nativex::server', 'Installs and configures glusterfs server.'
+recipe 'glusterfs-nativex::peer', 'Installs and configures glusterfs server as a peer.'
+recipe 'glusterfs-nativex::client', 'Installs and configures glusterfs client and mounts volumes.'
 
-depends  'yum'
+depends  "yum"
