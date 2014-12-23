2b139d2c7204ee29ee845a083d5c2cbc681ad1fe
Derek Bromenshenkel <derek.bromenshenkel@nativex.com>
2014-12-23 12:52:18 -0600

    Add support for stop/start a node with Ephemeral disks (make sure all the directories inside the ephemeral mount exist.)

--- cookbooks/dse-nativex/metadata.rb
+++ cookbooks/dse-nativex/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'derek.bromenshenkel@nativex.com'
 license          'All rights reserved'
 description      'Installs/Configures DataStax Enterprise'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
 depends "java", "= 1.24.0"
 depends "yum", "~> 3.0"
