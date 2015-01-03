01c5bb2115d1173caa062d0b19fa24533dfa248a
simplyadrian <simplyadrian@gmail.com>
2015-01-02 20:31:51 -0600

    update to role cookbooks

--- cookbooks/role-nas-nativex/metadata.rb
+++ cookbooks/role-nas-nativex/metadata.rb
@@ -5,3 +5,10 @@ license          'All rights reserved'
 description      'Installs/Configures role-nas-nativex'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+depends 'role-base-nativex'
+depends 'tuned-nativex::apply_profile'
+depends 'nfs::server4'
+depends 'blockdevice-nativex'
+depends 'blockdevice-nativex::tags'
+depends 'nfs-nativex'
