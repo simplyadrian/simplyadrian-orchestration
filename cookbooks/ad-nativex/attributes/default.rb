20d569d5ddadf80d9ec7e26b126730378b9c7c7b
Derek Bromenshenkel <derek.bromenshenkel@nativex.com>
2015-01-08 15:04:04 -0600

    Add environment (dev/prod/etc) and server classification (one off / web, etc) levels to the ad-nativex OU path.

--- cookbooks/ad-nativex/attributes/default.rb
+++ cookbooks/ad-nativex/attributes/default.rb
@@ -10,14 +10,14 @@ default['ad-nativex']['organizational_unit_level_3'] = 'Windows'
 default['ad-nativex']['organizational_unit_level_4'] = "#{node['environment']}"
 default['ad-nativex']['organizational_unit_level_5'] = 'One Off Servers'
 # OUPath
-default['ad-nativex']['oupath'] = "OU=#{node['ad-nativex']['organizational_unit_level_5']},"\
+default['ad-nativex']['oupath'] = "'OU=#{node['ad-nativex']['organizational_unit_level_5']},"\
                                   "OU=#{node['ad-nativex']['organizational_unit_level_4']},"\
                                   "OU=#{node['ad-nativex']['organizational_unit_level_3']},"\
                                   "OU=#{node['ad-nativex']['organizational_unit_level_2']},"\
                                   "OU=#{node['ad-nativex']['organizational_unit_level_1']},"\
                                   "OU=#{node['ad-nativex']['organizational_unit_level_0']},"\
                                   "DC=#{node['ad-nativex']['domain_component_level_1']},"\
-                                  "DC=#{node['ad-nativex']['domain_component_level_0']}"
+                                  "DC=#{node['ad-nativex']['domain_component_level_0']}'"
 # Site Name
 default['ad-nativex']['site_name'] = 'AMAZON'
 # Safe Mode Password
