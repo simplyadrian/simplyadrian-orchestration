26887eca60ed813edeabb897297786c3c22a027e
Derek Bromenshenkel <derek.bromenshenkel@nativex.com>
2015-01-07 18:02:08 -0600

    Componetize the pbis oupath. Allows for future flexibility while making it simpler to meet current requirements. [ISE-294]

--- cookbooks/pbis-nativex/attributes/default.rb
+++ cookbooks/pbis-nativex/attributes/default.rb
@@ -1,4 +1,13 @@
 # Domain Name
 default['pbis-nativex']['domain_name'] = 'teamfreeze.com'
-#OU
-default['pbis-nativex']['oupath'] = "OU=Linux,OU=UnknownRegion,OU=AWS Servers,OU=Computer Accounts,DC=teamfreeze,DC=com"
+# OU individual components
+default['pbis-nativex']['domain_component_level_0'] = 'com'
+default['pbis-nativex']['domain_component_level_1'] = 'teamfreeze'
+default['pbis-nativex']['organizational_unit_level_0'] = 'Computer Accounts'
+default['pbis-nativex']['organizational_unit_level_1'] = 'AWS Servers'
+default['pbis-nativex']['organizational_unit_level_2'] = 'UnknownRegion'
+default['pbis-nativex']['organizational_unit_level_3'] = 'Linux'
+default['pbis-nativex']['organizational_unit_level_4'] = "#{node['environment']}"
+default['pbis-nativex']['organizational_unit_level_5'] = 'One Off Servers'
+#Full OU path
+default['pbis-nativex']['oupath'] = "OU=#{node['pbis-nativex']['organizational_unit_level_5']},OU=#{node['pbis-nativex']['organizational_unit_level_4']},OU=#{node['pbis-nativex']['organizational_unit_level_3']},OU=#{node['pbis-nativex']['organizational_unit_level_2']},OU=#{node['pbis-nativex']['organizational_unit_level_1']},OU=#{node['pbis-nativex']['organizational_unit_level_0']},DC=#{node['pbis-nativex']['domain_component_level_1']},DC=#{node['pbis-nativex']['domain_component_level_0']}"
