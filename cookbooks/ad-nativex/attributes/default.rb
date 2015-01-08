ccb188dcfdb2e946d04e220db3e8621251888dcc
Derek Bromenshenkel <derek.bromenshenkel@nativex.com>
2015-01-08 10:30:38 -0600

    Componetize the ad oupath. Allows for future flexibility while making it simpler to meet current requirements. [ISE-293]

--- cookbooks/ad-nativex/attributes/default.rb
+++ cookbooks/ad-nativex/attributes/default.rb
@@ -1,7 +1,23 @@
 # Domain Name
 default['ad-nativex']['name'] = 'teamfreeze.com'
+# OU individual components
+default['ad-nativex']['domain_component_level_0'] = 'com'
+default['ad-nativex']['domain_component_level_1'] = 'teamfreeze'
+default['ad-nativex']['organizational_unit_level_0'] = 'Computer Accounts'
+default['ad-nativex']['organizational_unit_level_1'] = 'AWS Servers'
+default['ad-nativex']['organizational_unit_level_2'] = 'UnknownRegion'
+default['ad-nativex']['organizational_unit_level_3'] = 'Windows'
+default['ad-nativex']['organizational_unit_level_4'] = "#{node['environment']}"
+default['ad-nativex']['organizational_unit_level_5'] = 'One Off Servers'
 # OUPath
-default['ad-nativex']['oupath'] = "OU=Windows,OU=UnknownRegion,OU=AWS Servers,OU=Computer Accounts,DC=teamfreeze,DC=com"
+default['ad-nativex']['oupath'] = "OU=#{node['ad-nativex']['organizational_unit_level_5']},"\
+                                  "OU=#{node['ad-nativex']['organizational_unit_level_4']},"\
+                                  "OU=#{node['ad-nativex']['organizational_unit_level_3']},"\
+                                  "OU=#{node['ad-nativex']['organizational_unit_level_2']},"\
+                                  "OU=#{node['ad-nativex']['organizational_unit_level_1']},"\
+                                  "OU=#{node['ad-nativex']['organizational_unit_level_0']},"\
+                                  "DC=#{node['ad-nativex']['domain_component_level_1']},"\
+                                  "DC=#{node['ad-nativex']['domain_component_level_0']}"
 # Site Name
 default['ad-nativex']['site_name'] = 'AMAZON'
 # Safe Mode Password
