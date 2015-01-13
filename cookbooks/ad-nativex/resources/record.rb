54b26d1a3bc84ab8e5a749e85b6bb28fd28859c5
Derek Bromenshenkel <derek.bromenshenkel@nativex.com>
2015-01-13 10:32:56 -0600

    Moved ad-nativex to Berkshelf

--- cookbooks/ad-nativex/resources/record.rb
+++
@@ -1,7 +0,0 @@
-actions :create
-
-default_action :create
-
-attribute :ad_username, :kind_of => String
-attribute :ad_password, :kind_of => String
-attribute :ad_session_token, :kind_of => String
--- cookbooks/ad-nativex/resources/rename.rb
+++
@@ -1,7 +0,0 @@
-actions :rename
-default_action :rename
-
-attribute :name, :kind_of => String, :name_attribute => true
-attribute :hostname, :kind_of => String
-attribute :domain_user, :kind_of => String
-attribute :domain_pass, :kind_of => String
