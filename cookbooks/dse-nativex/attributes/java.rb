d7c3613c9b497a243eb74c036dd5b2598994a6a0
Derek Bromenshenkel <derek.bromenshenkel@nativex.com>
2015-01-13 10:55:54 -0600

    Moved dse-nativex to Berkshelf

--- cookbooks/dse-nativex/attributes/java.rb
+++
@@ -1,9 +0,0 @@
-default['java']['install_flavor'] = 'oracle'
-default['java']['jdk_version']    = '7'
-default['dse']['manage_java']     = true
-
-default['java']['jdk']['7']['bin_cmds'] = [ "appletviewer", "apt", "extcheck", "idlj", "jar", "jarsigner", "java", "javac",
-                                            "javadoc", "javafxpackager", "javah", "javap", "javaws", "jcmd", "jconsole", "jcontrol", "jdb",
-                                            "jhat", "jinfo", "jmap", "jps", "jrunscript", "jsadebugd", "jstack", "jstat", "jstatd", "jvisualvm",
-                                            "keytool", "native2ascii", "orbd", "pack200", "policytool", "rmic", "rmid", "rmiregistry",
-                                            "schemagen", "serialver", "servertool", "tnameserv", "unpack200", "wsgen", "wsimport", "xjc" ]
