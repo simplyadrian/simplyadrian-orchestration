38ac37c8c7e59e8c1a74be80689173eba3e55411
Derek Bromenshenkel <derek.bromenshenkel@nativex.com>
2015-01-12 21:57:30 -0600

    Fix naming of 'apache2-nativex' cookbook. It is only referenced in the 'role-apache-webserver-nativex' cookbook where it was already fixed.

--- cookbooks/apache2-nativex/metadata.rb
+++ cookbooks/apache2-nativex/metadata.rb
@@ -1,8 +1,8 @@
-name             'apache-nativex2'
+name             'apache2-nativex'
 maintainer       'NativeX'
 maintainer_email 'adrian.herrera@nativex.com'
 license          'All rights reserved'
-description      'Installs/Configures apache-nativex2'
+description      'Installs/Configures apache2-nativex'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
