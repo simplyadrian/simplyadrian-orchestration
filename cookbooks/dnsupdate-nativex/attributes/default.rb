45af00f05fd75635b248d9c6bc93db7d36ddc753
Derek Bromenshenkel <derek.bromenshenkel@nativex.com>
2015-01-13 10:55:45 -0600

    Moved dnsupdate-nativex to Berkshelf

--- cookbooks/dnsupdate-nativex/attributes/default.rb
+++
@@ -1,8 +0,0 @@
-#
-# Internal Domain Name
-default['dnsupdate-nativex']['int_domain'] = 'nativexintern.com'
-# Internal ZoneID
-default['dnsupdate-nativex']['int_zone_id'] = 'Z1NZHUXT02UQJ'
-# Record Type SOA, A, TXT, NS, CNAME, MX, PTR, SRV, SPF, AAAA.
-default['dnsupdate-nativex']['record_type'] = 'A'
-
