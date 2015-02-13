683a4d1a2328058ac26c00c6a47b3efa52285c7f
Derek Bromenshenkel <derek.bromenshenkel@nativex.com>
2015-02-13 17:39:05 -0600

    Created new role/role cookbook/environments to extend the production 'cdc-cass1' cluster out into AWS. [ISE-342]

---
+++ cookbooks/role-dse-cdccass1-aws-nativex/metadata.rb
@@ -0,0 +1,8 @@
+name             'role-dse-cdccass1-aws-nativex'
+maintainer       'NativeX'
+maintainer_email 'derek.bromenshenkel@nativex.com'
+license          'All rights reserved'
+description      'Installs/Configures role-dse-cdccass1-aws-nativex'
+version          '1.0.0'
+
+depends 'role-dse-nativex'
