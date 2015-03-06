8b3afd7725483a7486dcd2c2c26da51a9592495d
Derek Bromenshenkel <derek.bromenshenkel@nativex.com>
2015-03-06 17:00:55 -0600

    Create barebones role/environment/cookbook 'win-onpremise-base' to server as base role for Windows servers in CDC/STHO. [ISE-297]

---
+++ cookbooks/role-win-onpremise-base-nativex/metadata.rb
@@ -0,0 +1,8 @@
+name             'role-win-onpremise-base-nativex'
+maintainer       'NativeX'
+maintainer_email 'derek.bromenshenkel@nativex.com'
+license          'All rights reserved'
+description      'Installs/Configures role-win-onpremise-base-nativex'
+long_description 'Installs/Configures role-win-onpremise-base-nativex'
+version          '0.1.0'
+
---
+++ cookbooks/role-win-onpremise-base-nativex/recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: role-win-onpremise-base-nativex
+# Recipe:: default
+#
+# Copyright (C) 2015 NativeX
+#
+# All rights reserved - Do Not Redistribute
+#
