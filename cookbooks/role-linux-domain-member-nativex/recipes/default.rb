cee0f6f6423228a8dcfa75397d0ba16d6c96ba95
simplyadrian <simplyadrian@gmail.com>
2015-03-25 11:36:48 -0500

    ISE-329 initial RCB for linux domain member management

---
+++ cookbooks/role-linux-domain-member-nativex/recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: role-linux-domain-member-nativex
+# Recipe:: default
+#
+# Copyright 2015, simplyadrian
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe 'autofs-nativex'
