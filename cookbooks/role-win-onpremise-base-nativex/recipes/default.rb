6a8e3002fad549d3d31ba998fd81b1eb15def00d
Derek Bromenshenkel <derek.bromenshenkel@nativex.com>
2015-03-26 11:20:04 -0500

    Make chef-client service startup mode be 'Automatic (Delayed)' so that the service starts up successfully after reboot. [ISE-358]

--- cookbooks/role-win-onpremise-base-nativex/recipes/default.rb
+++ cookbooks/role-win-onpremise-base-nativex/recipes/default.rb
@@ -12,3 +12,15 @@ node.default['autopatch-nativex']['auto_reboot_enabled'] = true
 include_recipe "autopatch-nativex::default"
 include_recipe "chef-client::delete_validation"
 include_recipe "chef-client"
+
+# This is a hack of sorts to make the chef-client service start up mode be "Automatic (Delayed)".
+# The default is "Automatic". Without it delayed, the service fails to start on reboot.
+# See https://github.com/chef/chef/issues/2814
+registry_key 'HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\chef-client' do
+  values [{
+    :name => "DelayedAutostart",
+    :type => :dword,
+    :data => 1
+  }]
+  action :create
+end
