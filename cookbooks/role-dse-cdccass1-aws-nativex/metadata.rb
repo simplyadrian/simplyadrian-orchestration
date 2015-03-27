d7b67d5073cdc150b581b4f402b241201d514fae
Derek Bromenshenkel <derek.bromenshenkel@nativex.com>
2015-03-27 09:04:03 -0500

    Upversion DSE roles after fix for proper attribute precedence.

--- cookbooks/role-dse-cdccass1-aws-nativex/metadata.rb
+++ cookbooks/role-dse-cdccass1-aws-nativex/metadata.rb
@@ -3,6 +3,6 @@ maintainer       'NativeX'
 maintainer_email 'derek.bromenshenkel@nativex.com'
 license          'All rights reserved'
 description      'Installs/Configures role-dse-cdccass1-aws-nativex'
-version          '1.0.0'
+version          '1.0.1'
 
 depends 'role-dse-nativex'
