44586bf61587615f07766c272af196d3664724e5
Derek Bromenshenkel <derek.bromenshenkel@nativex.com>
2015-03-30 11:02:01 -0500

    Created skeletion for new cdccass1-onprem role to manage Chicago Cassandra nodes. [ISE-326]

---
+++ cookbooks/role-cdccass1-onprem-nativex/metadata.rb
@@ -0,0 +1,8 @@
+name             'role-cdccass1-onprem-nativex'
+maintainer       'NativeX'
+maintainer_email 'derek.bromenshenkel@nativex.com'
+license          'All rights reserved'
+description      'Installs/Configures role-cdccass1-onprem-nativex'
+long_description 'Installs/Configures role-cdccass1-onprem-nativex'
+version          '0.1.0'
+
---
+++ cookbooks/role-cdccass1-onprem-nativex/recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: role-cdccass1-onprem-nativex
+# Recipe:: default
+#
+# Copyright (C) 2015 NativeX
+#
+# All rights reserved - Do Not Redistribute
+#
