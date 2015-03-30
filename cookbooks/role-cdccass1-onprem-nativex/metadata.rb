01416d9e28e97a9dedbc4d09bafcdec78cb5b807
Derek Bromenshenkel <derek.bromenshenkel@nativex.com>
2015-03-30 11:14:40 -0500

    Add initial set of dependencies to role-cdccass1-onprem-nativex cookbook. [ISE-326]

--- cookbooks/role-cdccass1-onprem-nativex/metadata.rb
+++ cookbooks/role-cdccass1-onprem-nativex/metadata.rb
@@ -6,3 +6,5 @@ description      'Installs/Configures role-cdccass1-onprem-nativex'
 long_description 'Installs/Configures role-cdccass1-onprem-nativex'
 version          '0.1.0'
 
+depends "role-onpremise-base-nativex"
+depends "cron"
