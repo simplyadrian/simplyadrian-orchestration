c255039bf7d31362bc5be0456c35cc9277d80105
simplyadrian <simplyadrian@gmail.com>
2015-03-31 17:26:01 -0500

    ISE-329 adding more sudoers configuration attributes

--- cookbooks/role-base-nativex/recipes/default.rb
+++ cookbooks/role-base-nativex/recipes/default.rb
@@ -35,17 +35,23 @@ node.default['sshd']['sshd_config'] = {
   'SyslogFacility' => 'AUTHPRIV',
   'UseDNS' => 'no'
 }
-node.default['authorization']['sudo']['groups'] = ['admins']
-node.default['authorization']['sudo']['sudoers_defaults'] = [
-  '!visiblepw',
-  'env_reset',
-  'env_keep = "COLORS DISPLAY HOSTNAME HISTSIZE INPUTRC KDEDIR \
-               LS_COLORS MAIL PS1 PS2 QTDIR USERNAME \
-               LANG LC_ADDRESS LC_CTYPE LC_COLLATE LC_IDENTIFICATION \
-               LC_MEASUREMENT LC_MESSAGES LC_MONETARY LC_NAME LC_NUMERIC \
-               LC_PAPER LC_TELEPHONE LC_TIME LC_ALL LANGUAGE LINGUAS \
-               _XKB_CHARSET XAUTHORITY"'
+node.default['authorization']['sudo']['sudoers_defaults'] = [ 
+  'requiretty',
+  '!visiblepw', 
+  'env_reset', 
+  'env_keep = "COLORS DISPLAY HOSTNAME HISTSIZE INPUTRC KDEDIR LS_COLORS"', 
+  'env_keep += "MAIL PS1 PS2 QTDIR USERNAME LANG LC_ADDRESS LC_CTYPE"', 
+  'env_keep += "LC_COLLATE LC_IDENTIFICATION LC_MEASUREMENT LC_MESSAGES"', 
+  'env_keep += "LC_MONETARY LC_NAME LC_NUMERIC LC_PAPER LC_TELEPHONE"', 
+  'env_keep += "LC_TIME LC_ALL LANGUAGE LINGUAS _XKB_CHARSET XAUTHORITY"', 
+  'env_keep += "HOME"', 
+  'always_set_home', 
+  'secure_path = /sbin:/bin:/usr/sbin:/usr/bin' 
 ]
+node.default['authorization']['sudo']['groups'] = ['admins']
+node.default['authorization']['sudo']['groups'] = ['predictive_analytics']
+node.default['authorization']['sudo']['groups'] = ['it_backup']
+node.default['authorization']['sudo']['groups'] = ['mobilerelease']
 node.default['tuned']['active_profile'] = "virtual-guest"
 
 include_recipe 'ohai-nativex'
