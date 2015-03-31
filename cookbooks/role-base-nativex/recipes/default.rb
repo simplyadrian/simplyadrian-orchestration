22c2dc42d038f7cfaa821ac3bf64da53b9e4adeb
simplyadrian <simplyadrian@gmail.com>
2015-03-31 17:11:58 -0500

    ISE-329 adding more sudoers configuration attributes

--- cookbooks/role-base-nativex/recipes/default.rb
+++ cookbooks/role-base-nativex/recipes/default.rb
@@ -35,7 +35,17 @@ node.default['sshd']['sshd_config'] = {
   'SyslogFacility' => 'AUTHPRIV',
   'UseDNS' => 'no'
 }
-node.default['sudo']['groups'] = ['admins']
+node.default['authorization']['sudo']['groups'] = ['admins']
+node.default['authorization']['sudo']['sudoers_defaults'] = [
+  '!visiblepw',
+  'env_reset',
+  'env_keep = "COLORS DISPLAY HOSTNAME HISTSIZE INPUTRC KDEDIR \
+               LS_COLORS MAIL PS1 PS2 QTDIR USERNAME \
+               LANG LC_ADDRESS LC_CTYPE LC_COLLATE LC_IDENTIFICATION \
+               LC_MEASUREMENT LC_MESSAGES LC_MONETARY LC_NAME LC_NUMERIC \
+               LC_PAPER LC_TELEPHONE LC_TIME LC_ALL LANGUAGE LINGUAS \
+               _XKB_CHARSET XAUTHORITY"'
+]
 node.default['tuned']['active_profile'] = "virtual-guest"
 
 include_recipe 'ohai-nativex'
